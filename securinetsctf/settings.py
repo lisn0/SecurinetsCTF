03ed25a48e45711ea4a5e89f16c63d04e9af239d
lisn0 <ctf@securinets.s3ggcfz3lj3ublcrg5vr131pkb.bx.internal.cloudapp.net>
2019-11-08 16:34:05 +0000

    Ready to heroku this sucker in the face.

--- securinetsctf/settings.py
+++ securinetsctf/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import yaml
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -58,7 +59,6 @@ INSTALLED_APPS = [
     'ctf_framework',
     'django_slack_oauth',
     'rules.apps.AutodiscoverRulesConfig',
-    'herokuapp',
 ]
 
 AUTHENTICATION_BACKENDS = (
@@ -162,3 +162,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
